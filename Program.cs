762516688d7d52abb6cc3e05230c041de7123d1f
Kab-31-16 <Kab-31-16@KAB-31-16-PC>
2025-05-15 10:41:32 +0300

    123

--- Program.cs
+++ Program.cs
@@ -8,26 +8,9 @@ namespace prac41
 {
     internal class Program
     {
-        delegate double Nums(double a,double b);
-        static double Least(double a, double b)
-        {
-            return a<b ? b : a;
-        }
-        static double Greatest(double a, double b)
-        {
-            return a>b ? b : a;
-        }
         static void Main(string[] args)
         {
-            Console.WriteLine("Введите веществ число 1: ");
-            double a = double.Parse(Console.ReadLine());
-            Console.WriteLine("Введите веществ число 2: ");
-            double b = double.Parse(Console.ReadLine());
-            Nums numLeast = Least;
-            Nums numGreat = Greatest;
-            Console.WriteLine(numLeast(a, b));
-            Console.WriteLine(numGreat(a, b));
-
+            
         }
     }
 }
